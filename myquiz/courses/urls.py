874501cd29f377661e60f6844423a1b68af1df21
Syed Talha Ahmed Gardazi <atalha54@gmail.com>
2022-05-07 11:39:27 +0500

    Enrollment serializer added

--- myquiz/courses/urls.py
+++ myquiz/courses/urls.py
@@ -9,8 +9,13 @@ urlpatterns = [
     # url(r'^api/', views.course_list),
     # url(r'^api/course_by_id/(?P<pk>[0-9]+)$', views.course_detail),
     
-    ###############################################################
+    # _____________ Courses URLs__________________#
     url(r'^api/courses$', views.course_list),
     url(r'^api/courses/(?P<pk>[0-9]+)$', views.course_detail),
-    url(r'^api/courses/published$', views.course_list_active)
+    url(r'^api/courses/published$', views.course_list_active),
+    
+    # _____________ Enrollment URLs__________________#
+    url(r'^api/enrollment$', views.enrollment_list),
+    url(r'^api/enrollment/(?P<pk>[0-9]+)$', views.enrollment_detail),
+    url(r'^api/enrollment/published$', views.Enrollment_list_active),
 ]
